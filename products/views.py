45817e27e3abadaf2692dd82c8902a4e51a02b62
Aibek2201 <aibekalmabekuly@gmail.com>
2023-02-15 03:20:19 +0600

    added custom permissions

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 
+from products.permissions import IsMe
 from utils import mixins
 from products import models, serializers
 
@@ -16,4 +17,14 @@ class ProductViewSet(mixins.ActionSerializerMixin, ModelViewSet):
     }
     serializer_class = serializers.ProductSerializer
     queryset = models.Product.objects.all()
-    permission_classes = (IsAuthenticated,)
+
+    def get_permissions(self):
+        if self.action in ('list', 'retrieve'):
+            return AllowAny,
+
+        return IsMe,
+
+    def list(self, request, *args, **kwargs):
+        print(request.user)
+        return super().list(request, *args, **kwargs)
+
