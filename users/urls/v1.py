a25c5263379ba580e155a58418f6dbb9ef75d847
Aibek2201 <aibekalmabekuly@gmail.com>
2023-02-18 13:01:44 +0600

    changed code style

--- users/urls/v1.py
+++ users/urls/v1.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenVerifyView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenVerifyView, TokenRefreshView
 
 from users import views
 
@@ -10,4 +10,4 @@ urlpatterns = [
     path('users/token/', views.UserViewSet.as_view({'post': 'create_token'})),
     path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('users/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-]
+    ]
