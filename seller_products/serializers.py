4e22a85dad34a2edc903df7d0863d35601d89dd4
Aibek2201 <aibekalmabekuly@gmail.com>
2023-03-01 01:32:34 +0600

    changed app configs

--- seller_products/serializers.py
+++ seller_products/serializers.py
@@ -3,6 +3,19 @@ from products import serializers as product_serializers
 from . import models
 
 
+class SellerProductCreateSerializer(serializers.ModelSerializer):
+
+
+    class Meta:
+        model = models.Seller_Product
+        fields = (
+            'product',
+            'amount',
+            'amount_currency',
+            'is_active',
+        )
+
+
 class SellerProductSerializer(serializers.ModelSerializer):
     product = product_serializers.ProductSerializer()
 
--- seller_products/views.py
+++ seller_products/views.py
@@ -1,8 +1,15 @@
 from rest_framework.viewsets import ModelViewSet
 
+from utils import mixins
 from . import serializers, models
 
 
-class SellerProductViewSet(ModelViewSet):
+class SellerProductViewSet(mixins.ActionSerializerMixin, ModelViewSet):
+    ACTION_SERIALIZERS = {
+        'create':serializers.SellerProductCreateSerializer,
+    }
     serializer_class = serializers.SellerProductSerializer
     queryset = models.Seller_Product.objects.select_related('seller', 'product')
+
+    def perform_create(self, serializer):
+        serializer.save(seller=self.request.user)
